4540fc46dee71e25d885d447c98dd6d144a9fb11
Vast Gui <sakurajimamai2020@qq.com>
2023-03-02 21:28:47 +0800

    update(AdapterDiffUtil.md):添加AdapterDiffUtil

---
+++ libraries/VastAdapter/src/main/kotlin/com/ave/vastgui/adapter/widget/AdapterDiffUtil.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2022 VastGui guihy2019@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ave.vastgui.adapter.widget
+
+import androidx.recyclerview.widget.DiffUtil
+
+// Author: Vast Gui
+// Email: guihy2019@gmail.com
+// Date: 2023/2/27
+// Description: 
+// Documentation:
+// Reference:
+
+/**
+ * Because of the use of [AdapterItemWrapper], the original [DiffUtil] will
+ * be inconvenient to use, so [AdapterDiffUtil] is designed to replace it.
+ *
+ * @param T The class of the list item.
+ */
+abstract class AdapterDiffUtil<T, R : AdapterItemWrapper<T>> : DiffUtil.ItemCallback<R>() {
+
+    /** @see newAreContentsTheSame */
+    final override fun areContentsTheSame(oldItem: R, newItem: R): Boolean {
+        return newAreContentsTheSame(oldItem.getData(), newItem.getData())
+    }
+
+    /** @see newAreItemsTheSame */
+    final override fun areItemsTheSame(oldItem: R, newItem: R): Boolean {
+        return newAreItemsTheSame(oldItem.getData(), newItem.getData())
+    }
+
+    abstract fun newAreContentsTheSame(oldItem: T, newItem: T): Boolean
+
+    abstract fun newAreItemsTheSame(oldItem: T, newItem: T): Boolean
+
+}
