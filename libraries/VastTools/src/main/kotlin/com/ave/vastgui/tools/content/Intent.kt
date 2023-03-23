23d17a7e0b5871f0f729ccd703ec526759166c30
Vast Gui <sakurajimamai2020@qq.com>
2023-03-23 14:53:08 +0800

    feat(Bundle.kt,Intent.kt):对于Bundle和Intent获取数据进行优化

---
+++ libraries/VastTools/src/main/kotlin/com/ave/vastgui/tools/content/Intent.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2022 VastGui guihy2019@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ave.vastgui.tools.content
+
+import android.content.Intent
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.os.Build
+import com.ave.vastgui.core.extension.cast
+import java.io.Serializable
+
+// Author: Vast Gui
+// Email: guihy2019@gmail.com
+// Date: 2023/3/22
+// Description: 
+// Documentation:
+// Reference:
+
+/**
+ * Retrieve extended data from the intent.
+ *
+ * @param key The name of the desired item.
+ * @param callback As the return value if the retrieve extended data from the intent is null.
+ *
+ * @see [Intent.getSerializableExtra]
+ */
+inline fun <reified T : Serializable> Intent.getSerializableExtra(key: String, callback: T): T {
+    return getSerializableExtra(this, key) ?: callback
+}
+
+/** @see [Intent.getSerializableExtra]. */
+inline fun <reified T : Serializable> getSerializableExtra(intent: Intent, key: String): T? {
+    val data = if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
+        @Suppress("DEPRECATION")
+        intent.getSerializableExtra(key)
+    } else {
+        intent.getSerializableExtra(key, T::class.java)
+    }
+    return if(null != data){ cast(data) } else null
+}
---
+++ libraries/VastTools/src/main/kotlin/com/ave/vastgui/tools/os/Bundle.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2022 VastGui guihy2019@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ave.vastgui.tools.os
+
+import android.os.Build
+import android.os.Bundle
+import android.os.Parcelable
+
+// Author: Vast Gui
+// Email: guihy2019@gmail.com
+// Date: 2023/3/22
+// Description: 
+// Documentation:
+// Reference:
+
+/**
+ * Returns the value associated with the given key.
+ *
+ * @param key A string.
+ * @param callback As the return value if the return the value associated
+ *     with the given key is null.
+ * @see [Bundle.getParcelable]
+ */
+inline fun <reified T : Parcelable> Bundle.getParcelable(key: String, callback: T) =
+    getParcelable(this, key) ?: callback
+
+/** @see [Bundle.getParcelable]. */
+inline fun <reified T : Parcelable> getParcelable(bundle: Bundle, key: String) =
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
+        @Suppress("DEPRECATION")
+        bundle.getParcelable(key)
+    } else {
+        bundle.getParcelable(key, T::class.java)
+    }
