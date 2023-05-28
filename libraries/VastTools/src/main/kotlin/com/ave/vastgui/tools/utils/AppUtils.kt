86b1821afc8db29012688c151430f99b520446ea
Vast Gui <sakurajimamai2020@qq.com>
2023-05-28 22:55:48 +0800

    fix(AppUtils.kt):修复获取应用图标因为AdaptiveIconDrawable无法转换为BitmapDrawable的异常，完善默认值设置

--- libraries/VastTools/src/main/kotlin/com/ave/vastgui/tools/utils/AppUtils.kt
+++ libraries/VastTools/src/main/kotlin/com/ave/vastgui/tools/utils/AppUtils.kt
@@ -19,10 +19,13 @@ package com.ave.vastgui.tools.utils
 import android.content.pm.ApplicationInfo
 import android.content.pm.PackageInfo
 import android.content.pm.PackageManager
+import android.content.res.Resources
 import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.graphics.drawable.BitmapDrawable
 import android.os.Build
 import androidx.annotation.RequiresApi
+import com.ave.vastgui.core.extension.cast
 import com.ave.vastgui.tools.bean.Component3
 import com.ave.vastgui.tools.helper.ContextHelper
 
@@ -35,41 +38,59 @@ import com.ave.vastgui.tools.helper.ContextHelper
 object AppUtils {
 
     /**
-     * @return app name,like VastUtilsSampleDemo.
+     * Get app name.
+     *
+     * @param callback The default value.
+     * @return App name,like VastUtilsSampleDemo or [callback] if [callback] is
+     *     not null,throw exception otherwise.
+     * @throws Resources.NotFoundException
+     * @throws PackageManager.NameNotFoundException
+     * @since 0.5.1
      */
     @JvmStatic
-    fun getAppName(): String? {
-        try {
-            val labelRes = getPackageInfo().applicationInfo.labelRes
-            return ContextHelper.getAppContext().resources.getString(labelRes)
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-        return null
+    @Throws(Resources.NotFoundException::class, PackageManager.NameNotFoundException::class)
+    fun getAppName(callback: String? = null): String = try {
+        val labelRes = getPackageInfo().applicationInfo.labelRes
+        ContextHelper.getAppContext().resources.getString(labelRes)
+    } catch (exception: Exception) {
+        callback ?: throw exception
     }
 
-    /**
-     * Returns the version name of the current application,like 1.0.
-     */
-    @JvmStatic
-    fun getVersionName(): String? {
-        try {
-            return getPackageInfo().versionName
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-        return null
-    }
 
     /**
-     * Returns the version code of the current application,like 1.
+     * Get version name.
+     *
+     * @param callback The default value.
+     * @return The version name of the current application,like 1.0 or callback
+     *     if callback is not null,throw exception otherwise.
+     * @throws PackageManager.NameNotFoundException
+     * @since 0.5.1
      */
     @JvmStatic
-    fun getVersionCode() =
+    @Throws(PackageManager.NameNotFoundException::class)
+    fun getVersionName(callback: String? = null): String = try {
+        getPackageInfo().versionName
+    } catch (exception: Exception) {
+        callback ?: throw exception
+    }
+
+
+    /**
+     * Get version code.
+     *
+     * @param callback The default value.
+     * @return The version code of the current application,like 1 or callback
+     *     if callback is not null,throw exception otherwise.
+     * @throws PackageManager.NameNotFoundException
+     * @since 0.5.1
+     */
+    @JvmStatic
+    @Throws(PackageManager.NameNotFoundException::class)
+    fun getVersionCode(callback: Int? = null) =
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-            getVersionCodeApi28Above()
+            getVersionCodeApi28Above(callback)
         } else {
-            getVersionCodeApi28Down()
+            getVersionCodeApi28Down(callback)
         }
 
 
@@ -77,115 +98,130 @@ object AppUtils {
      * Get VersionCode (in Api 28 Above)
      *
      * @return The version code of the current application.
+     * @since 0.5.1
      */
-    @Synchronized
     @RequiresApi(Build.VERSION_CODES.P)
-    private fun getVersionCodeApi28Above(): Int {
-        try {
-            return getPackageInfo().longVersionCode.toInt()
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-        return 0
+    @Throws(PackageManager.NameNotFoundException::class)
+    private fun getVersionCodeApi28Above(callback: Int? = null) = try {
+        getPackageInfo().longVersionCode.toInt()
+    } catch (exception: Exception) {
+        callback ?: throw exception
     }
 
     /**
      * Get VersionCode (in Api 28 Down)
      *
      * @return The version code of the current application.
+     * @since 0.5.1
      */
-    private fun getVersionCodeApi28Down(): Int {
-        try {
-            @Suppress("DEPRECATION")
-            return getPackageInfo().versionCode
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-        return 0
+    @Throws(PackageManager.NameNotFoundException::class)
+    private fun getVersionCodeApi28Down(callback: Int? = null): Int = try {
+        @Suppress("DEPRECATION") getPackageInfo().versionCode
+    } catch (exception: Exception) {
+        callback ?: throw exception
     }
 
-    /**
-     * Returns the package name of the application,like
-     * com.gcode.vastutils.
-     */
-    @JvmStatic
-    fun getPackageName(): String? {
-        try {
-            return getPackageInfo().packageName
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-        return null
-    }
 
     /**
-     * Returns the icon of the application.
+     * Get package name.
+     *
+     * @param callback The default value.
+     * @return The package name of the application,like com.gcode.vastutils or
+     *     callback if callback is not null,throw exception otherwise.
+     * @throws PackageManager.NameNotFoundException
+     * @since 0.5.1
      */
     @JvmStatic
-    fun getAppBitmap(): Bitmap? {
-        var packageManager: PackageManager? = null
-        var applicationInfo: ApplicationInfo?
-        try {
-            packageManager = ContextHelper.getAppContext().applicationContext
-                .packageManager
-            applicationInfo = getApplicationInfo()
-        } catch (e: PackageManager.NameNotFoundException) {
-            applicationInfo = null
-        }
-        val d =
-            packageManager!!.getApplicationIcon(applicationInfo!!)
-        val bd = d as BitmapDrawable
-        return bd.bitmap
+    @Throws(PackageManager.NameNotFoundException::class)
+    fun getPackageName(callback: String? = null): String = try {
+        getPackageInfo().packageName
+    } catch (exception: Exception) {
+        callback ?: throw exception
     }
 
+
     /**
-     * Returns true if the app is debuggable.false otherwise.
+     * Get app icon bitmap.
+     *
+     * @return The icon of the application or [callback] if [callback] is not
+     *     null,throw exception otherwise.
+     * @throws PackageManager.NameNotFoundException
+     * @since 0.5.1
      */
     @JvmStatic
-    fun getAppDebug(): Boolean {
-        return try {
-            val info = ContextHelper.getAppContext().applicationInfo
-            info.flags and ApplicationInfo.FLAG_DEBUGGABLE != 0
-        } catch (e: java.lang.Exception) {
-            false
+    @Throws(PackageManager.NameNotFoundException::class)
+    fun getAppBitmap(callback: Bitmap? = null): Bitmap = try {
+        val packageManager = ContextHelper.getAppContext().applicationContext.packageManager
+        val applicationInfo = getApplicationInfo()
+        val drawable = packageManager.getApplicationIcon(applicationInfo)
+        // Fix AdaptiveIconDrawable cannot be cast to BitmapDrawable.
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val bitmap =
+                Bitmap.createBitmap(drawable.intrinsicWidth, drawable.intrinsicHeight, Bitmap.Config.ARGB_8888)
+            val canvas = Canvas(bitmap)
+            drawable.setBounds(0, 0, canvas.width, canvas.height)
+            drawable.draw(canvas)
+            BitmapDrawable(ContextHelper.getAppContext().resources, bitmap).bitmap
+        } else {
+            cast<BitmapDrawable>(drawable).bitmap
         }
+    } catch (exception: Exception) {
+        callback ?: throw exception
     }
 
+
     /**
-     * Return the maxMemory, freeMemory, totalMemory of the application in turn.
+     * Get app debug mode.
+     *
+     * @return True if the app is debuggable.false otherwise.
+     */
+    @JvmStatic
+    fun getAppDebug() = try {
+        val info = ContextHelper.getAppContext().applicationInfo
+        info.flags and ApplicationInfo.FLAG_DEBUGGABLE != 0
+    } catch (exception: Exception) {
+        false
+    }
+
+
+    /**
+     * Get memory information.
+     *
+     * @return The maxMemory, freeMemory, totalMemory of the application in
+     *     turn.
      */
     @JvmStatic
     fun getMemoryInfo(): Component3<Double, Double, Double> {
         return Component3(
-            Runtime.getRuntime().maxMemory()*1.0/(1024*1024),
-            Runtime.getRuntime().freeMemory()*1.0/(1024*1024),
-            Runtime.getRuntime().totalMemory()*1.0/(1024*1024)
+            Runtime.getRuntime().maxMemory() * 1.0 / (1024 * 1024),
+            Runtime.getRuntime().freeMemory() * 1.0 / (1024 * 1024),
+            Runtime.getRuntime().totalMemory() * 1.0 / (1024 * 1024)
         )
     }
 
+    @Throws(PackageManager.NameNotFoundException::class)
     private fun getPackageInfo(): PackageInfo {
         val packageManager: PackageManager = ContextHelper.getAppContext().packageManager
-        return if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             packageManager.getPackageInfo(
                 ContextHelper.getAppContext().packageName, PackageManager.PackageInfoFlags.of(0)
             )
         } else {
-            @Suppress("DEPRECATION")
-            packageManager.getPackageInfo(
+            @Suppress("DEPRECATION") packageManager.getPackageInfo(
                 ContextHelper.getAppContext().packageName, 0
             )
         }
     }
 
+    @Throws(PackageManager.NameNotFoundException::class)
     private fun getApplicationInfo(): ApplicationInfo {
         val packageManager: PackageManager = ContextHelper.getAppContext().packageManager
-        return if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             packageManager.getApplicationInfo(
                 ContextHelper.getAppContext().packageName, PackageManager.ApplicationInfoFlags.of(0)
             )
         } else {
-            @Suppress("DEPRECATION")
-            packageManager.getApplicationInfo(
+            @Suppress("DEPRECATION") packageManager.getApplicationInfo(
                 ContextHelper.getAppContext().packageName, 0
             )
         }
