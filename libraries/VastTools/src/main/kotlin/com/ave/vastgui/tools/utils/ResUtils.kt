5be98aa7e06fb05eb0c1544631c5b47b5ca9e2a9
Vast Gui <sakurajimamai2020@qq.com>
2023-05-29 13:49:13 +0800

    feat(ResUtils.kt):ResUtils完善默认值

--- libraries/VastTools/src/main/kotlin/com/ave/vastgui/tools/utils/ResUtils.kt
+++ libraries/VastTools/src/main/kotlin/com/ave/vastgui/tools/utils/ResUtils.kt
@@ -18,8 +18,10 @@ package com.ave.vastgui.tools.utils
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.res.Resources
 import android.content.res.Resources.NotFoundException
 import android.graphics.drawable.Drawable
+import androidx.annotation.ColorInt
 import androidx.annotation.ColorRes
 import androidx.annotation.DimenRes
 import androidx.annotation.DrawableRes
@@ -34,126 +36,102 @@ import com.ave.vastgui.tools.helper.ContextHelper
 
 object ResUtils {
 
-    /** Resources not found tag. */
-    const val RESOURCES_NOT_FOUND = 0
-
-    /** Error color hex string. */
-    private const val ERROR_COLOR = "#c0392b"
-
-    /** Get string by [id]. */
-    @JvmStatic
-    @JvmOverloads
-    fun getString(@StringRes id: Int, context: Context = ContextHelper.getAppContext()): String =
-        context.resources.getString(id)
-
     /**
-     * Get drawable by [name].
+     * Get string by [id] or use [callback] as the default value, throw
+     * exception otherwise.
      *
-     * @param name the name of the drawable.
-     * @return the drawable resource corresponding to the [name], otherwise
-     *     [callback].
+     * @throws Resources.NotFoundException
+     * @since 0.5.1
      */
     @JvmStatic
     @JvmOverloads
-    fun getDrawable(
-        name: String,
-        callback: Drawable,
-        context: Context = ContextHelper.getAppContext()
-    ): Drawable {
-        return getDrawable(name, context) ?: callback
+    @Throws(Resources.NotFoundException::class)
+    fun getString(@StringRes id: Int, callback: String? = null): String = try {
+        ContextHelper.getAppContext().resources.getString(id)
+    } catch (exception: Exception) {
+        callback ?: throw exception
     }
 
     /**
      * Get drawable by [name].
      *
      * @param name the name of the drawable.
-     * @return the drawable resource corresponding to the [name], otherwise
-     *     null if the resource does not exist.
+     * @return The drawable resource get by [name], or use [callback] as the
+     *     default value, otherwise throw exception.
+     * @throws Resources.NotFoundException
+     * @since 0.5.1
      */
     @SuppressLint("DiscouragedApi")
     @JvmStatic
     @JvmOverloads
-    fun getDrawable(name: String, context: Context = ContextHelper.getAppContext()): Drawable? {
+    @Throws(Resources.NotFoundException::class)
+    fun getDrawable(name: String, callback: Drawable? = null): Drawable = try {
+        val context = ContextHelper.getAppContext()
         val resId = context.resources.getIdentifier(name, "drawable", context.packageName)
-        return if (RESOURCES_NOT_FOUND == resId) {
-            null
-        } else {
-            ResourcesCompat.getDrawable(context.resources, resId, context.theme)
-        }
+        ResourcesCompat.getDrawable(context.resources, resId, context.theme)!!
+    } catch (exception: Exception) {
+        callback ?: throw exception
     }
 
     /**
      * Get drawable by [resId].
      *
      * @param resId the resource id of the drawable.
-     * @return the drawable resource corresponding to the [resId], otherwise
-     *     [callback].
+     * @return the drawable resource get by [resId], or use [callback] as the
+     *     default value, otherwise throw exception.
+     * @throws Resources.NotFoundException
+     * @since 0.5.1
      */
     @JvmStatic
     @JvmOverloads
+    @Throws(Resources.NotFoundException::class)
     fun getDrawable(
-        @DrawableRes resId: Int,
-        callback: Drawable,
-        context: Context = ContextHelper.getAppContext()
-    ): Drawable {
-        return getDrawable(resId, context) ?: callback
+        @DrawableRes resId: Int, callback: Drawable? = null
+    ): Drawable = try {
+        val context = ContextHelper.getAppContext()
+        ResourcesCompat.getDrawable(context.resources, resId, context.theme)!!
+    } catch (exception: NotFoundException) {
+        callback ?: throw exception
     }
 
-    /**
-     * Get drawable by [resId].
-     *
-     * @param resId the resource id of the drawable.
-     * @return the drawable resource corresponding to the [resId], otherwise
-     *     null if the resource does not exist.
-     */
-    @JvmStatic
-    @JvmOverloads
-    fun getDrawable(
-        @DrawableRes resId: Int,
-        context: Context = ContextHelper.getAppContext()
-    ): Drawable? {
-        return try {
-            ResourcesCompat.getDrawable(context.resources, resId, context.theme)
-        } catch (ex: NotFoundException) {
-            null
-        }
-    }
 
     /**
-     * A single color value in the form 0xAARRGGBB. Or color int of
-     * [ERROR_COLOR] will be return if resource is not exist.
+     * A single color value in the form 0xAARRGGBB. Or color int of [callback]
+     * will be return if resource is not exist, otherwise throw exception.
      *
      * @param id the resource id of color.
+     * @throws Resources.NotFoundException
+     * @since 0.5.1
      */
     @JvmStatic
     @JvmOverloads
-    fun getColor(@ColorRes id: Int, context: Context = ContextHelper.getAppContext()): Int {
-        return try {
-            context.getColor(id)
-        } catch (e: NotFoundException) {
-            ColorUtils.colorHex2Int(ERROR_COLOR)
-        }
+    @Throws(Resources.NotFoundException::class)
+    fun getColor(@ColorRes id: Int, @ColorInt callback: Int? = null): Int = try {
+        val context: Context = ContextHelper.getAppContext()
+        context.getColor(id)
+    } catch (exception: Exception) {
+        callback ?: throw exception
     }
 
     /**
      * Retrieve a dimensional for a particular resource ID for use as an offset
      * in raw pixels. The returned value is converted to integer pixels for
      * you. An offset conversion involves simply truncating the base value to
-     * an integer. Otherwise 0 will be return.
+     * an integer. or use [callback] as the default value, otherwise throw
+     * exception.
      *
      * @param id the resource id of dimension.
+     * @throws Resources.NotFoundException
+     * @since 0.5.1
      */
     @JvmStatic
     @JvmOverloads
-    fun getDimensionPixelOffset(
-        @DimenRes id: Int,
-        context: Context = ContextHelper.getAppContext()
-    ): Int {
-        return try {
-            context.resources.getDimensionPixelOffset(id)
-        } catch (ex: NotFoundException) {
-            0
-        }
+    @Throws(Resources.NotFoundException::class)
+    fun getDimensionPixelOffset(@DimenRes id: Int, callback: Int? = null): Int = try {
+        val context: Context = ContextHelper.getAppContext()
+        context.resources.getDimensionPixelOffset(id)
+    } catch (exception: NotFoundException) {
+        callback ?: throw exception
     }
 
 }
