0c743645950af636b8710b896df726f4acf0ac59
Vast Gui <sakurajimamai2020@qq.com>
2023-08-28 12:04:12 +0800

    update:更新视图绑定拓展示例代码

--- app/src/main/java/com/ave/vastgui/app/activity/vbexample/VbActivity.kt
+++ app/src/main/java/com/ave/vastgui/app/activity/vbexample/VbActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.ave.vastgui.app.R
 import com.ave.vastgui.app.databinding.ActivityMyBinding
 import com.ave.vastgui.tools.viewbinding.viewBinding
 
@@ -44,6 +45,38 @@ class VbActivity2 : Activity() {
     override fun onDestroy() {
         super.onDestroy()
         viewBindingProperty.clear()
+    }
+
+}
+
+class VbActivity3 : ComponentActivity(R.layout.activity_my) {
+
+    private val mBinding by viewBinding(ActivityMyBinding::bind)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mBinding.personRv.adapter = VbAdapter(persons)
+        mBinding.personRv.layoutManager = LinearLayoutManager(this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+}
+
+class VbActivity4 : ComponentActivity(R.layout.activity_my) {
+
+    private val mBinding by viewBinding(ActivityMyBinding::bind, R.id.activity_my_root)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mBinding.personRv.adapter = VbAdapter(persons)
+        mBinding.personRv.layoutManager = LinearLayoutManager(this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 
 }
