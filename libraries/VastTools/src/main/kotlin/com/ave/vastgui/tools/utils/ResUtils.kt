1d7f0cfd60ed0c3482448b8d71bef7120b2298ec
Vast Gui <sakurajimamai2020@qq.com>
2023-09-02 16:20:12 +0800

    feat(ResUtils):新增findByContext和findByResources用来查找资源

--- libraries/VastTools/src/main/kotlin/com/ave/vastgui/tools/utils/ResUtils.kt
+++ libraries/VastTools/src/main/kotlin/com/ave/vastgui/tools/utils/ResUtils.kt
@@ -14,19 +14,12 @@
  * limitations under the License.
  */
 
+@file:JvmName("ResUtilsKt")
+
 package com.ave.vastgui.tools.utils
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.content.res.Resources
-import android.content.res.Resources.NotFoundException
-import android.graphics.drawable.Drawable
-import androidx.annotation.ColorInt
-import androidx.annotation.ColorRes
-import androidx.annotation.DimenRes
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
-import androidx.core.content.res.ResourcesCompat
 import com.ave.vastgui.tools.helper.ContextHelper
 
 // Author: Vast Gui
@@ -34,104 +27,58 @@ import com.ave.vastgui.tools.helper.ContextHelper
 // Date: 2022/6/19
 // Documentation: https://ave.entropy2020.cn/documents/VastTools/core-topics/app-resources/ResUtils/
 
-object ResUtils {
-
-    /**
-     * Get string by [id] or use [callback] as the default value, throw
-     * exception otherwise.
-     *
-     * @throws Resources.NotFoundException
-     * @since 0.5.1
-     */
-    @JvmStatic
-    @JvmOverloads
-    @Throws(NotFoundException::class)
-    fun getString(@StringRes id: Int, callback: String? = null): String = try {
-        ContextHelper.getAppContext().resources.getString(id)
+/**
+ * Using the context provided by [ContextHelper.getAppContext] and [method]
+ * to find the required resources.
+ *
+ * ```kotlin
+ * val drawable = findByContext {
+ *     AppCompatResources.getDrawable(this, R.drawable.android_logo)
+ * }
+ * ```
+ *
+ * @param callback The default value if [findByContext] can't get the
+ *     resource by method.
+ * @param method Method of providing resources
+ * @param T Resource Type.
+ * @since 0.5.2
+ */
+@Throws(Exception::class)
+@JvmOverloads
+inline fun <T : Any> findByContext(callback: T? = null, method: Context.() -> T?): T =
+    try {
+        method(ContextHelper.getAppContext()) ?: callback
+        ?: throw NullPointerException("Can't get the resource and the callback is null.")
     } catch (exception: Exception) {
         callback ?: throw exception
     }
 
-    /**
-     * Get drawable by [name].
-     *
-     * @param name the name of the drawable.
-     * @return The drawable resource get by [name], or use [callback] as the
-     *     default value, otherwise throw exception.
-     * @throws Resources.NotFoundException
-     * @since 0.5.1
-     */
-    @SuppressLint("DiscouragedApi")
-    @JvmStatic
-    @JvmOverloads
-    @Throws(NotFoundException::class)
-    fun getDrawable(name: String, callback: Drawable? = null): Drawable = try {
-        val context = ContextHelper.getAppContext()
-        val resId = context.resources.getIdentifier(name, "drawable", context.packageName)
-        ResourcesCompat.getDrawable(context.resources, resId, context.theme)!!
-    } catch (exception: Exception) {
-        callback ?: throw exception
-    }
-
-    /**
-     * Get drawable by [resId].
-     *
-     * @param resId the resource id of the drawable.
-     * @return the drawable resource get by [resId], or use [callback] as the
-     *     default value, otherwise throw exception.
-     * @throws Resources.NotFoundException
-     * @since 0.5.1
-     */
-    @JvmStatic
-    @JvmOverloads
-    @Throws(NotFoundException::class)
-    fun getDrawable(
-        @DrawableRes resId: Int, callback: Drawable? = null
-    ): Drawable = try {
-        val context = ContextHelper.getAppContext()
-        ResourcesCompat.getDrawable(context.resources, resId, context.theme)!!
-    } catch (exception: NotFoundException) {
-        callback ?: throw exception
-    }
-
-
-    /**
-     * A single color value in the form 0xAARRGGBB. Or color int of [callback]
-     * will be return if resource is not exist, otherwise throw exception.
-     *
-     * @param id the resource id of color.
-     * @throws Resources.NotFoundException
-     * @since 0.5.1
-     */
-    @JvmStatic
-    @JvmOverloads
-    @Throws(NotFoundException::class)
-    fun getColor(@ColorRes id: Int, @ColorInt callback: Int? = null): Int = try {
-        val context: Context = ContextHelper.getAppContext()
-        context.getColor(id)
-    } catch (exception: Exception) {
-        callback ?: throw exception
-    }
-
-    /**
-     * Retrieve a dimensional for a particular resource ID for use as an offset
-     * in raw pixels. The returned value is converted to integer pixels for
-     * you. An offset conversion involves simply truncating the base value to
-     * an integer. or use [callback] as the default value, otherwise throw
-     * exception.
-     *
-     * @param id the resource id of dimension.
-     * @throws Resources.NotFoundException
-     * @since 0.5.1
-     */
-    @JvmStatic
-    @JvmOverloads
-    @Throws(NotFoundException::class)
-    fun getDimensionPixelOffset(@DimenRes id: Int, callback: Int? = null): Int = try {
-        val context: Context = ContextHelper.getAppContext()
-        context.resources.getDimensionPixelOffset(id)
-    } catch (exception: NotFoundException) {
-        callback ?: throw exception
-    }
-
+/**
+ * Using the resources and theme provided by [ContextHelper.getAppContext]
+ * and [method] to find the required resources.
+ *
+ * ```kotlin
+ * val drawable = findByResources { res, theme ->
+ *     ResourcesCompat.getDrawable(res, R.drawable.android_logo, theme)
+ * }
+ * ```
+ *
+ * @param callback The default value if [findByResources] can't get the
+ *     resource by method.
+ * @param method Method of providing resources
+ * @param T Resource Type.
+ * @since 0.5.2
+ */
+@Throws(Exception::class)
+@JvmOverloads
+inline fun <T : Any> findByResources(
+    callback: T? = null,
+    method: (Resources, Resources.Theme) -> T?
+): T = try {
+    val resources = ContextHelper.getAppContext().resources
+    val theme = ContextHelper.getAppContext().theme
+    method(resources, theme) ?: callback
+    ?: throw NullPointerException("Can't get the resource and the callback is null.")
+} catch (exception: Exception) {
+    callback ?: throw exception
 }
