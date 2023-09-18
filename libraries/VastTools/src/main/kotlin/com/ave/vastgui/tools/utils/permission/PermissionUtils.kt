608765f47382265f324a9690e569a30d86e9bb7e
Vast Gui <sakurajimamai2020@qq.com>
2023-09-18 21:15:06 +0800

    update(VastTools):更新PermissionUtils文档链接

--- libraries/VastTools/src/main/kotlin/com/ave/vastgui/tools/utils/permission/PermissionUtils.kt
+++ libraries/VastTools/src/main/kotlin/com/ave/vastgui/tools/utils/permission/PermissionUtils.kt
@@ -25,7 +25,7 @@ import androidx.fragment.app.Fragment
 // Author: Vast Gui
 // Email: guihy2019@gmail.com
 // Date: 2023/4/5
-// Documentation: https://ave.entropy2020.cn/documents/VastTools/core-topics/permission/Permission/
+// Documentation: https://ave.entropy2020.cn/documents/VastTools/core-topics/permission/permission/
 
 const val DENIED = "DENIED"
 const val EXPLAINED = "EXPLAINED"
