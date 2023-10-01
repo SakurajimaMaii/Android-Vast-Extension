81a2e9f82e7818ca906e9dbba6a15fc649a541e2
Vast Gui <sakurajimamai2020@qq.com>
2023-10-01 13:07:34 +0800

    update(App):更新ArcProgressView示例代码

--- app/src/main/java/com/ave/vastgui/app/activity/view/ArcProgressViewActivity.kt
+++ app/src/main/java/com/ave/vastgui/app/activity/view/ArcProgressViewActivity.kt
@@ -18,17 +18,20 @@ package com.ave.vastgui.app.activity.view
 
 import android.graphics.LinearGradient
 import android.graphics.Shader
-import android.os.Build
 import android.os.Bundle
-import androidx.annotation.RequiresApi
 import com.ave.vastgui.app.activity.log.mLogFactory
 import com.ave.vastgui.app.databinding.ActivityArcProgressViewBinding
 import com.ave.vastgui.tools.activity.VastVbActivity
+import com.ave.vastgui.tools.graphics.BmpUtils
 import com.ave.vastgui.tools.manager.filemgr.FileMgr
 import com.ave.vastgui.tools.utils.ColorUtils
+import com.ave.vastgui.tools.utils.DensityUtils.DP
 import com.ave.vastgui.tools.utils.download.DLManager
 import com.ave.vastgui.tools.utils.download.DLTask
 import com.ave.vastgui.tools.view.extension.refreshWithInvalidate
+import com.ave.vastgui.tools.view.extension.viewSnapshot
+import com.ave.vastgui.tools.view.toast.SimpleToast
+import java.io.File
 
 // Author: Vast Gui 
 // Email: guihy2019@gmail.com
@@ -40,7 +43,6 @@ class ArcProgressViewActivity : VastVbActivity<ActivityArcProgressViewBinding>()
     private val logger = mLogFactory.getLog(ArcProgressViewActivity::class.java)
     private lateinit var downloadTask: DLTask
 
-    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -54,25 +56,10 @@ class ArcProgressViewActivity : VastVbActivity<ActivityArcProgressViewBinding>()
             ColorUtils.colorHex2Int("#B709F4")
         )
         val pos = floatArrayOf(
-            1f / 7,
-            2f / 7,
-            3f / 7,
-            4f / 7,
-            5f / 7,
-            6f / 7,
-            1f
+            1f / 7, 2f / 7, 3f / 7, 4f / 7, 5f / 7, 6f / 7, 1f
         )
 
-//        val colors = intArrayOf(
-//            ColorUtils.colorHex2Int("#ED4C67"),
-//            ColorUtils.colorHex2Int("#FFFFFF"),
-//        )
-//        val pos = floatArrayOf(
-//            0.3f,
-//            1f
-//        )
-
-        getBinding().downloadCv.apply {
+        getBinding().arcProgressView.apply {
             setProgressShader(
                 LinearGradient(
                     -700f, 0f, 700f, 0f,
@@ -80,6 +67,17 @@ class ArcProgressViewActivity : VastVbActivity<ActivityArcProgressViewBinding>()
                     Shader.TileMode.CLAMP
                 )
             )
+            mProgressWidth = 10f.DP
+        }
+
+        getBinding().arcProgressView.setOnClickListener {
+            val bitmap = viewSnapshot(it)
+            BmpUtils.saveBitmapAsFile(
+                bitmap = bitmap,
+                File(FileMgr.appInternalFilesDir(), "width_10.jpg")
+            )?.apply {
+                SimpleToast.showShortMsg("截图${name}已保存")
+            }
         }
 
         getBinding().download.setOnClickListener {
@@ -106,9 +104,9 @@ class ArcProgressViewActivity : VastVbActivity<ActivityArcProgressViewBinding>()
             .setSaveDir(FileMgr.appInternalFilesDir().path)
             .setListener {
                 onDownloading = {
-                    getBinding().downloadCv.refreshWithInvalidate {
+                    getBinding().arcProgressView.refreshWithInvalidate {
                         setCurrentProgress(
-                            it.rate * getBinding().downloadCv.mMaximumProgress
+                            it.rate * getBinding().arcProgressView.mMaximumProgress
                         )
                     }
                 }
@@ -117,13 +115,13 @@ class ArcProgressViewActivity : VastVbActivity<ActivityArcProgressViewBinding>()
                 }
                 onSuccess = {
                     logger.i("download success.")
-                    getBinding().downloadCv.refreshWithInvalidate {
-                        setCurrentProgress(getBinding().downloadCv.mMaximumProgress)
+                    getBinding().arcProgressView.refreshWithInvalidate {
+                        setCurrentProgress(getBinding().arcProgressView.mMaximumProgress)
                     }
                 }
                 onCancel = {
                     logger.i("download cancel.")
-                    getBinding().downloadCv.refreshWithInvalidate {
+                    getBinding().arcProgressView.refreshWithInvalidate {
                         resetProgress()
                     }
                 }
