30f461e3806569af3de3d30c6ce877d18a3cb2fb
Vast Gui <sakurajimamai2020@qq.com>
2024-05-05 15:56:48 +0800

    update(Project):更新了项目依赖

--- app-compose/build.gradle.kts
+++ app-compose/build.gradle.kts
@@ -23,12 +23,12 @@ plugins {
 
 android {
     namespace = "com.ave.vastgui.appcompose"
-    compileSdk = 34
+    compileSdk = libs.versions.compileSdk.get().toInt()
 
     defaultConfig {
         applicationId = "com.ave.vastgui.appcompose"
-        minSdk = 23
-        targetSdk = 34
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
         versionCode = 1
         versionName = "1.0"
 
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,12 +25,12 @@ plugins {
 
 android {
     compileSdkPreview = "UpsideDownCake"
-    compileSdk = 34
+    compileSdk = libs.versions.compileSdk.get().toInt()
 
     defaultConfig {
         applicationId = "com.ave.vastgui.app"
-        minSdk = 23
-        targetSdk = 34
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
         versionCode = 1
         versionName = "1.0"
 
