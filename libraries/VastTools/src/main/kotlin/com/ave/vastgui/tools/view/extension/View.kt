e977f318538938089e473034f41447f3ab68cdb9
Vast Gui <sakurajimamai2020@qq.com>
2024-05-10 21:02:15 +0800

    feat(VastTools):新增键盘工具类

--- libraries/VastTools/src/main/kotlin/com/ave/vastgui/tools/view/extension/View.kt
+++ libraries/VastTools/src/main/kotlin/com/ave/vastgui/tools/view/extension/View.kt
@@ -1,24 +1,101 @@
 package com.ave.vastgui.tools.view.extension
 
+import android.app.Service
+import android.util.Log
+import android.view.MotionEvent
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.core.view.isGone
 import androidx.core.view.isInvisible
 import androidx.core.view.isVisible
+import com.ave.vastgui.core.extension.cast
+import com.ave.vastgui.tools.log.LogUtil
 
 // Author: Vast Gui
 // Email: guihy2019@gmail.com
 // Date: 2024/3/30
 // Documentation: https://ave.entropy2020.cn/documents/VastTools/core-topics/ui/extension/view/
 
-// ---------- visibility ----------
+// ---------- Visibility ----------
+/**
+ * @since 1.2.1
+ */
 fun View.gone() {
     isGone = true
 }
 
+/**
+ * @since 1.2.1
+ */
 fun View.visible() {
     isVisible = true
 }
 
+/**
+ * @since 1.2.1
+ */
 fun View.invisible() {
     isInvisible = true
+}
+
+// ---------- KeyBroad ----------
+/**
+ * Show KeyBroad.
+ *
+ * @since 1.3.1
+ */
+fun EditText.showKeyBroad() {
+    val imm = runCatching {
+        cast<InputMethodManager>(context.getSystemService(Service.INPUT_METHOD_SERVICE))
+    }.getOrElse {
+        Log.e(LogUtil.TAG, "Get InputMethodManager encounter exception:${it.stackTraceToString()}")
+        null
+    }
+    requestFocus()
+    imm?.showSoftInput(this, 0)
+}
+
+/**
+ * Hide KeyBroad.
+ *
+ * @since 1.3.1
+ */
+fun EditText.hideKeyBroad() {
+    val imm = runCatching {
+        cast<InputMethodManager>(context.getSystemService(Service.INPUT_METHOD_SERVICE))
+    }.getOrElse {
+        Log.e(LogUtil.TAG, "Get InputMethodManager encounter exception:${it.stackTraceToString()}")
+        null
+    }
+    imm?.hideSoftInputFromWindow(windowToken, 0)
+}
+
+/**
+ * Return true if current [event] coordinate is outside the EditView, false otherwise.
+ * Here is an example:
+ * ```kotlin
+ * // In Activity.kt
+ * override fun onTouchEvent(event: MotionEvent?): Boolean {
+ *     if (MotionEvent.ACTION_DOWN == event?.action) {
+ *         val view = currentFocus
+ *         if (null != view && view is EditText) {
+ *             if (view.isShouldHideKeyBroad(event)) {
+ *                 view.hideKeyBroad()
+ *             }
+ *         }
+ *     }
+ *     return super.onTouchEvent(event)
+ * }
+ * ```
+ *
+ * @since 1.3.1
+ */
+fun EditText.isShouldHideKeyBroad(event: MotionEvent): Boolean {
+    val topLeft = intArrayOf(0, 0)
+    getLocationOnScreen(topLeft)
+    val left = topLeft[0]
+    val top = topLeft[1]
+    return !(event.rawX in left.toFloat()..(left + measuredWidth).toFloat() &&
+            event.rawY in top.toFloat()..(top + measuredHeight).toFloat())
 }
