a9cfc3183eab663f9f5efcbd284f3f4c4121c479
Vast Gui <sakurajimamai2020@qq.com>
2024-06-06 01:05:00 +0800

    update(app):更新视图绑定和反射示例代码

---
+++ app/src/main/kotlin/com/ave/vastgui/app/net/WanAndroidApiService.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2021-2024 VastGui
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ave.vastgui.app.net
+
+import com.ave.vastgui.app.net.model.Articles
+import retrofit2.http.GET
+
+
+// Author: Vast Gui
+// Email: guihy2019@gmail.com
+// Date: 2024/6/4
+// Description: WanAndroid 开放 Api
+// Reference: https://www.wanandroid.com/blog/show/2
+
+interface WanAndroidApiService {
+
+    @GET("/article/list/1/json")
+    suspend fun getArticles(): Articles
+
+}
---
+++ app/src/main/kotlin/com/ave/vastgui/app/net/model/Articles.kt
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2021-2024 VastGui
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ave.vastgui.app.net.model
+
+
+// Author: Vast Gui
+// Email: guihy2019@gmail.com
+// Date: 2024/6/4
+// Description: 首页文章列表
+// Reference: https://www.wanandroid.com/blog/show/2
+
+data class Articles(
+    val `data`: Data,
+    val errorCode: Int,
+    val errorMsg: String
+){
+    data class Data(
+        val curPage: Int,
+        val datas: List<Article>,
+        val offset: Int,
+        val over: Boolean,
+        val pageCount: Int,
+        val size: Int,
+        val total: Int
+    )
+
+    data class Article(
+        val adminAdd: Boolean,
+        val apkLink: String,
+        val audit: Int,
+        val author: String,
+        val canEdit: Boolean,
+        val chapterId: Int,
+        val chapterName: String,
+        val collect: Boolean,
+        val courseId: Int,
+        val desc: String,
+        val descMd: String,
+        val envelopePic: String,
+        val fresh: Boolean,
+        val host: String,
+        val id: Int,
+        val isAdminAdd: Boolean,
+        val link: String,
+        val niceDate: String,
+        val niceShareDate: String,
+        val origin: String,
+        val prefix: String,
+        val projectLink: String,
+        val publishTime: Long,
+        val realSuperChapterId: Int,
+        val selfVisible: Int,
+        val shareDate: Long,
+        val shareUser: String,
+        val superChapterId: Int,
+        val superChapterName: String,
+        val tags: List<Tag>,
+        val title: String,
+        val type: Int,
+        val userId: Int,
+        val visible: Int,
+        val zan: Int
+    )
+
+    data class Tag(
+        val name: String,
+        val url: String
+    )
+}
