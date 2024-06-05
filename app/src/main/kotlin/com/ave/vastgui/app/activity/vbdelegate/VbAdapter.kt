55f591299bb1e379017813507922787661b91b48
Vast Gui <sakurajimamai2020@qq.com>
2024-06-06 01:24:55 +0800

    update(app):更新视图绑定和反射示例代码

--- app/src/main/kotlin/com/ave/vastgui/app/activity/vbdelegate/VbAdapter.kt
+++ app/src/main/kotlin/com/ave/vastgui/app/activity/vbdelegate/VbAdapter.kt
@@ -5,8 +5,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.ave.vastgui.app.R
-import com.ave.vastgui.app.adapter.entity.Person
-import com.ave.vastgui.app.databinding.ItemPersonBinding
+import com.ave.vastgui.app.databinding.ItemArticleBinding
+import com.ave.vastgui.app.net.model.Articles
 import com.ave.vastgui.tools.viewbinding.viewBinding
 
 // Author: Vast Gui
@@ -14,54 +14,49 @@ import com.ave.vastgui.tools.viewbinding.viewBinding
 // Date: 2023/8/26
 // Documentation: https://ave.entropy2020.cn/documents/VastTools/architecture-components/ui-layer-libraries/view-bind/vb-delegate/#viewholder
 
-val persons = listOf(Person("张", "三"), Person("李", "四"), Person("王", "五"))
+class ArticleAdpt(private val data: MutableList<Articles.Article> = ArrayList()) :
+    RecyclerView.Adapter<ArticleAdpt.ArticleVH>() {
 
-class VbAdapter(private val data: List<Person>) : RecyclerView.Adapter<VbAdapter.MyViewHolder>() {
+    class ArticleVH(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        // 使用方式 1
+        private val binding by viewBinding(ItemArticleBinding::bind, R.id.article_root)
 
-    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val binding by viewBinding(ItemPersonBinding::bind)
-        val name = binding.name
-        val sentence = binding.sentence
+        // 使用方式 2
+        // private val binding by viewBinding(ItemArticleBinding::bind)
+        val title = binding.articleTitle
+        val shareUser = binding.articleShareUser
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleVH {
         val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.item_image_default, parent, false)
-
-        return MyViewHolder(view)
+            .inflate(R.layout.item_article, parent, false)
+        return ArticleVH(view)
     }
 
     override fun getItemCount() = data.size
 
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val person = data[position]
-        holder.name.text = person.name
-        holder.sentence.text = person.sentence
+    override fun onBindViewHolder(holder: ArticleVH, position: Int) {
+        val article = data[position]
+        holder.title.text = article.title
+        holder.shareUser.text = article.shareUser
     }
 
-}
-
-class VbAdapter2(private val data: List<Person>) : RecyclerView.Adapter<VbAdapter2.MyViewHolder>() {
-
-    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val binding by viewBinding(ItemPersonBinding::bind, R.id.item_textview_root)
-        val name = binding.name
-        val sentence = binding.sentence
+    /**
+     * 添加文章
+     */
+    fun addArticles(articles: List<Articles.Article>) {
+        val length = articles.size
+        data.addAll(itemCount, articles)
+        notifyItemRangeInserted(itemCount, itemCount + length)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.item_image_default, parent, false)
-
-        return MyViewHolder(view)
-    }
-
-    override fun getItemCount() = data.size
-
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val person = data[position]
-        holder.name.text = person.name
-        holder.sentence.text = person.sentence
+    /**
+     * 清空所有文章
+     */
+    fun clearAllArticles() {
+        val length = itemCount
+        data.clear()
+        notifyItemRangeChanged(0, length)
     }
 
 }
