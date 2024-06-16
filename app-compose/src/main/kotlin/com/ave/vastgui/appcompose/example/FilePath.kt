92843189f2939fa875a6d8ed98b034ab3e2544bc
Vast Gui <sakurajimamai2020@qq.com>
2024-06-16 15:56:48 +0800

    update(app-compose):更新示例代码

--- app-compose/src/main/kotlin/com/ave/vastgui/appcompose/example/FilePath.kt
+++ app-compose/src/main/kotlin/com/ave/vastgui/appcompose/example/FilePath.kt
@@ -19,6 +19,7 @@ package com.ave.vastgui.appcompose.example
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
