0309d9efb7e976a28dd6dadb7bb2a14f72a229c2
ywnkm <pkazusa1412@gmail.com>
2024-06-22 12:04:35 +0800

    impl pipeline core

---
+++ libraries/core/src/main/kotlin/com/ave/vastgui/core/pipeline/PipelinePhaseRelation.kt
@@ -0,0 +1,10 @@
+package com.ave.vastgui.core.pipeline
+
+internal sealed class PipelinePhaseRelation {
+
+    class After(val relativeTo: PipelinePhase) : PipelinePhaseRelation()
+
+    class Before(val relativeTo: PipelinePhase) : PipelinePhaseRelation()
+
+    data object Last : PipelinePhaseRelation()
+}
