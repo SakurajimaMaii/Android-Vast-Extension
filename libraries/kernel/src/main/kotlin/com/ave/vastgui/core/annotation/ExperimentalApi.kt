c732ca2e5433fd0158c566da8fcb7c19be363bac
Vast Gui <sakurajimamai2020@qq.com>
2025-03-12 00:44:23 +0800

    feat(core):新增字符串本地化， ASCII-UNICODE 转换工具类，文件排序比较器以及实验性 Api 注解

---
+++ libraries/kernel/src/main/kotlin/com/ave/vastgui/core/annotation/ExperimentalApi.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2021-2025 VastGui
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ave.vastgui.core.annotation
+
+// Author: Vast Gui
+// Email: guihy2019@gmail.com
+// Date: 2025/3/11
+
+/**
+ * The behavior of such API may be changed or the API may be removed
+ * completely in any further release.
+ *
+ * @since 0.1.4
+ */
+@RequiresOptIn(level = RequiresOptIn.Level.ERROR)
+@Retention(AnnotationRetention.BINARY)
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.CLASS)
+@MustBeDocumented
+internal annotation class ExperimentalApi
---
+++ libraries/kernel/src/main/kotlin/com/ave/vastgui/core/text/StringLocale.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2021-2025 VastGui
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ave.vastgui.core.text
+
+import java.util.regex.Pattern
+
+// Author: Vast Gui
+// Email: guihy2019@gmail.com
+// Date: 2025/3/9
+
+/**
+ * Chinese pattern, click
+ * [Character classes in regular expressions](https://learn.microsoft.com/en-us/dotnet/standard/base-types/character-classes-in-regular-expressions)
+ * to get more information.
+ *
+ * @since 0.1.4
+ */
+val ChinesePattern: Pattern = Pattern.compile("[\\u4E00-\\u9FBF]+")
+
+/**
+ * Return `true` if the [str] chinese characters.
+ *
+ * @since 0.1.4
+ */
+fun isChinese(str: String?): Boolean {
+    if (str.isNullOrBlank()) return false
+    return ChinesePattern.matcher(str.trim()).find()
+}
