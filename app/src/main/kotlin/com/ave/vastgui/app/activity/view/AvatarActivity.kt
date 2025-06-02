2f4862f855360c9ceb9fc300462896bf20a6a150
Vast Gui <sakurajimamai2020@qq.com>
2025-06-02 23:48:35 +0800

    update(app):更新 Avatar 和 AvatarGroup 示例代码

--- app/src/main/kotlin/com/ave/vastgui/app/activity/view/AvatarActivity.kt
+++ app/src/main/kotlin/com/ave/vastgui/app/activity/view/AvatarActivity.kt
@@ -21,9 +21,12 @@ import android.view.ViewGroup.LayoutParams
 import androidx.activity.ComponentActivity
 import com.ave.vastgui.app.R
 import com.ave.vastgui.app.databinding.ActivityAvatarBinding
+import com.ave.vastgui.app.log.logFactory
+import com.ave.vastgui.tools.utils.DensityUtils.DP
 import com.ave.vastgui.tools.view.avatar.Avatar
 import com.ave.vastgui.tools.view.avatar.AvatarGroup
 import com.ave.vastgui.tools.viewbinding.viewBinding
+import kotlin.random.Random
 
 // Author: Vast Gui
 // Email: guihy2019@gmail.com
@@ -33,19 +36,26 @@ import com.ave.vastgui.tools.viewbinding.viewBinding
 
 class AvatarActivity : ComponentActivity(R.layout.activity_avatar) {
 
-    val mBinding by viewBinding(ActivityAvatarBinding::bind)
+    private val logger = logFactory("AvatarActivity")
+    private val binding by viewBinding(ActivityAvatarBinding::bind)
+    private val radius: Float
+        get() = Random(System.currentTimeMillis()).nextInt(0, 20).toFloat().DP
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val mAvatar = Avatar(this).apply {
+        val avatar = Avatar(this).apply {
             setAvatar(R.drawable.img_avatar_sample_2)
         }
 
-        mBinding.avatarGroupStart.addView(
-            mAvatar, LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
-        )
-
-        mBinding.avatarGroupEnd.setOverlapFrom(AvatarGroup.END)
+        binding.avatarGroupStart.addView(avatar, LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT))
+        binding.avatarGroupEnd.setOverlapFrom(AvatarGroup.END)
+        binding.root.setOnClickListener {
+            logger.d("点击 root")
+            binding.avatarGroupStart.setOverlapFrom(AvatarGroup.Companion.END)
+            binding.avatarGroupStart.setShape(1 - binding.avatarGroupStart.shape)
+            binding.avatarGroupStart.strokeWidth = 10f.DP
+            binding.avatarGroupStart.cornerRadius = radius.also { logger.d("切换的半径是 $it") }
+        }
     }
 
 }
