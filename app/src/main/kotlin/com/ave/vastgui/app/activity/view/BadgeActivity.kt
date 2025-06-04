28f650bb3e23012b7228df85b9cd677ebda9a4e1
Vast <sakurajimamai2020@qq.com>
2025-06-04 18:40:01 +0800

    update(app):更新徽标组件示例代码

---
+++ app/src/main/kotlin/com/ave/vastgui/app/activity/view/BadgeActivity.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2021-2025 VastGui
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ave.vastgui.app.activity.view
+
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import com.ave.vastgui.app.R
+import com.ave.vastgui.app.databinding.ActivityBadgeBinding
+import com.ave.vastgui.app.fragment.view.BadgeLayoutFragment
+import com.ave.vastgui.app.log.logFactory
+import com.ave.vastgui.tools.viewbinding.viewBinding
+
+// Author: Vast Gui
+// Email: guihy2019@gmail.com
+// Date: 2023/9/11
+// Documentation: https://sakurajimamaii.github.io/AVE-DOC/documents/tools/core-topics/ui/badge/description/
+
+class BadgeActivity : FragmentActivity(R.layout.activity_badge) {
+
+    private val logger = logFactory(BadgeActivity::class.java)
+
+    private val binding by viewBinding(ActivityBadgeBinding::bind)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            replace<BadgeLayoutFragment>(binding.container.id)
+        }
+    }
+
+}
