c8656c178149dfb3a44336b335461fe142aef04a
Vast <sakurajimamai2020@qq.com>
2025-06-10 17:55:31 +0800

    update(app):更新 BreadCrumbLayout 示例代码

--- app/src/main/kotlin/com/ave/vastgui/app/activity/view/BreadcrumbActivity.kt
+++ app/src/main/kotlin/com/ave/vastgui/app/activity/view/BreadcrumbActivity.kt
@@ -20,8 +20,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import com.ave.vastgui.app.R
 import com.ave.vastgui.app.databinding.ActivityBreadcrumbBinding
+import com.ave.vastgui.tools.utils.DensityUtils.SP
+import com.ave.vastgui.tools.utils.color
 import com.ave.vastgui.tools.view.breadcrumb.BreadCrumb
+import com.ave.vastgui.tools.view.toast.SimpleToast
 import com.ave.vastgui.tools.viewbinding.viewBinding
+import java.util.Stack
 
 // Author: Vast Gui
 // Email: guihy2019@gmail.com
@@ -36,13 +40,58 @@ class BreadcrumbActivity : ComponentActivity(R.layout.activity_breadcrumb) {
 
     private val items = arrayOf("首页", "当这个页面标题很长很长很长时需要省略", "详情页")
 
+    private val textColors by lazy {
+        intArrayOf(color(R.color.honeydew), color(R.color.lightslategray), color(R.color.lime))
+    }
+
+    private var textColorsIndex = 0
+
+    private val icons = intArrayOf(R.drawable.ic_breadcrumb_interval_icon,
+        com.ave.vastgui.tools.R.drawable.ic_breadcrumb_default_interval_icon)
+
+    private var iconIndex = 0
+
+    private val breadcrumbIds = Stack<Int>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        items.forEach { binding.breadCrumbView.addBreadCrumbItem(BreadCrumb(it)) }
+        items.forEach {
+            binding.breadCrumbLayout.addItem(BreadCrumb(it)).also { id ->
+                breadcrumbIds.push(id)
+            }
+        }
 
+        // 添加导航路径
         binding.addItemBtn.setOnClickListener {
-            binding.breadCrumbView.addBreadCrumbItem(BreadCrumb("${index++}"))
+            binding.breadCrumbLayout.addItem(BreadCrumb("${index++}")).also { id ->
+                breadcrumbIds.push(id)
+            }
+        }
+
+        // 移除导航路径
+        binding.removeItemBtn.setOnClickListener {
+            if(breadcrumbIds.isEmpty()) return@setOnClickListener
+            binding.breadCrumbLayout.removeItem(breadcrumbIds.pop())
+        }
+
+        // 切换导航字体颜色
+        binding.switchTextColorBtn.setOnClickListener {
+            binding.breadCrumbLayout.setTextColor(textColors[(textColorsIndex++) % textColors.size])
+        }
+
+        // 切换导航字体大小
+        binding.textSizeSlider.addOnChangeListener { _, value, _ ->
+            binding.breadCrumbLayout.setTextSize(value.SP)
+        }
+
+        // 切换分隔符
+        binding.switchInternalIconBtn.setOnClickListener {
+            binding.breadCrumbLayout.setIntervalIcon(icons[(iconIndex++) % icons.size])
+        }
+
+        binding.breadCrumbLayout.addOnBreadCrumbClickListener { _, id ->
+            SimpleToast.showShortMsg(id.toString())
         }
     }
 
