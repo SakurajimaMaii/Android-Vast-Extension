38cb92238340c5d3f0b9cb4cef3d34a844320395
Vast <sakurajimamai2020@qq.com>
2025-06-13 18:34:16 +0800

    update(app):更行横向进度条示例代码

--- app/src/main/kotlin/com/ave/vastgui/app/activity/view/HorizontalProgressViewActivity.kt
+++ app/src/main/kotlin/com/ave/vastgui/app/activity/view/HorizontalProgressViewActivity.kt
@@ -17,11 +17,14 @@
 package com.ave.vastgui.app.activity.view
 
 import android.os.Bundle
-import android.widget.SeekBar
 import androidx.activity.ComponentActivity
+import androidx.core.content.ContextCompat
 import com.ave.vastgui.app.R
 import com.ave.vastgui.app.databinding.ActivityHorizontalProgressViewBinding
-import com.ave.vastgui.tools.view.extension.refreshWithInvalidate
+import com.ave.vastgui.app.log.logFactory
+import com.ave.vastgui.tools.utils.ColorUtils
+import com.ave.vastgui.tools.utils.DensityUtils
+import com.ave.vastgui.tools.utils.DensityUtils.DP
 import com.ave.vastgui.tools.viewbinding.viewBinding
 
 // Author: Vast Gui
@@ -30,10 +33,33 @@ import com.ave.vastgui.tools.viewbinding.viewBinding
 
 class HorizontalProgressViewActivity : ComponentActivity() {
 
+    private val logcat = logFactory(HorizontalProgressViewActivity::class.java)
+
     private val binding by viewBinding(ActivityHorizontalProgressViewBinding::inflate)
 
-    private val gradientImages = arrayOf(R.drawable.img_gradient_256dp, R.drawable.img_horizontal_pv_foreground, 0)
-    private var gradientIndex = 0
+    private val gradientImages = arrayOf(R.drawable.img_horizontal_pv_background,
+        R.drawable.img_horizontal_pv_foreground,
+        R.drawable.ic_cat_256dp, 0)
+
+    private var backgroundIndex = 0
+
+    private var progressIndex = 0
+
+    private val colors = intArrayOf(
+        ColorUtils.colorHex2Int("#55efc4"),
+        ColorUtils.colorHex2Int("#81ecec"),
+        ColorUtils.colorHex2Int("#6c5ce7"),
+        ColorUtils.colorHex2Int("#0984e3"),
+        ColorUtils.colorHex2Int("#fdcb6e"),
+        ColorUtils.colorHex2Int("#e17055"),
+        ColorUtils.colorHex2Int("#636e72")
+    )
+
+    private var progressColorIndex = 0
+
+    private var progressBackgroundColorIndex = 0
+
+    private var boxColorIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,8 +71,44 @@ class HorizontalProgressViewActivity : ComponentActivity() {
             binding.lineTextPv.currentProgress = value
         }
 
-        binding.horizontalPvSwitchBackground.setOnClickListener {
-            binding.horizontalPv.setProgressDrawable(gradientImages[(gradientIndex++) % gradientImages.size])
+        binding.horizontalPvSwitchBackgroundBtn.setOnClickListener {
+            // val index = gradientIndex
+            // binding.horizontalPv.setProgressDrawable(gradientImages[(index) % gradientImages.size])
+            // gradientIndex++
+
+            val image = runCatching { ContextCompat.getDrawable(this, gradientImages[(backgroundIndex++) % gradientImages.size]) }.getOrNull()
+            binding.horizontalPv.setProgressBkDrawable(image)
+        }
+
+        binding.horizontalPvSwitchProgressBtn.setOnClickListener {
+            val image = runCatching { ContextCompat.getDrawable(this, gradientImages[(progressIndex++) % gradientImages.size]) }.getOrNull()
+            binding.horizontalPv.setProgressDrawable(image)
+        }
+
+        binding.horizontalPvSwitchProgressColorBtn.setOnClickListener {
+            binding.horizontalPv.progressColor = colors[(progressColorIndex++) % colors.size]
+        }
+
+        binding.horizontalPvSwitchBackgroundColorBtn.setOnClickListener {
+            binding.horizontalPv.progressBackgroundColor = colors[(progressBackgroundColorIndex++) % colors.size]
+        }
+
+        binding.horizontalPvSwitchTextBoxColorBtn.setOnClickListener {
+            val color = colors[(boxColorIndex++) % colors.size]
+            binding.horizontalTextPv.textBoxColor = color
+            binding.lineTextPv.textBoxColor = color
+        }
+
+        binding.textMarginSlider.value = DensityUtils.px2dp(binding.horizontalTextPv.textMargin)
+        binding.textMarginSlider.addOnChangeListener { _, value, _ ->
+            binding.horizontalTextPv.textMargin = value.DP
+            binding.lineTextPv.textMargin = value.DP
+        }
+
+        binding.progressHeightSlider.value = DensityUtils.px2dp(binding.horizontalTextPv.progressHeight)
+        binding.progressHeightSlider.addOnChangeListener { _, value, _ ->
+            binding.horizontalTextPv.progressHeight = value.DP
+            binding.lineTextPv.progressHeight = value.DP
         }
     }
 
